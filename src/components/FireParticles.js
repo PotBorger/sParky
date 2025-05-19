7b7c1234dd1b4d07aeddc39f69577208c2b9666a
PotBorger <khanhtrinh232005@gmail.com>
2025-05-19 13:25:07 -0600

    Thay background

--- src/components/FireParticles.js
+++ src/components/FireParticles.js
@@ -1,12 +1,14 @@
 export default function FireParticles() {
   return (
     <div className="fire-particles">
-      {[...Array(8)].map((_, i) => (
+      {[...Array(6)].map((_, i) => (
         <div
           key={i}
           className="particle"
           style={{
-            left: `${20 + Math.random() * 80}%`,
+            left: `${
+              i % 2 === 0 ? 50 + 0.5 * 80 - i * 4 : 0 + 0.5 * 40 + i * 4
+            }%`,
             top: `${20 + Math.random() * 80}%`,
             animationDuration: `${3 + Math.random() * 5}s`,
             animationDelay: `${Math.random() * 2}s`,
