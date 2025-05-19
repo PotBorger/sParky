aa0de922a559b060e339df288558bed829356627
PotBorger <khanhtrinh232005@gmail.com>
2025-05-19 13:33:41 -0600

    Thay coord

--- src/components/FireParticles.js
+++ src/components/FireParticles.js
@@ -6,10 +6,10 @@ export default function FireParticles() {
           key={i}
           className="particle"
           style={{
-            left: `${
-              i % 2 === 0 ? 50 + 0.5 * 80 - i * 4 : 0 + 0.5 * 40 + i * 4
+            left: `${i % 2 === 0 ? 90 - i * 4 : 10 + i * 2}%`,
+            top: `${
+              i % 2 === 0 ? 50 + 0.5 * 40 - i * 3 : 0 + 0.5 * 40 + i * 8
             }%`,
-            top: `${20 + Math.random() * 80}%`,
             animationDuration: `${3 + Math.random() * 5}s`,
             animationDelay: `${Math.random() * 2}s`,
           }}
